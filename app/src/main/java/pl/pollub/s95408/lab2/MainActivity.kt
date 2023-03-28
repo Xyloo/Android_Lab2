64adcae620f4cd836fb750f37f2f63736f529502
Xylo <cezary2000@gmail.com>
2023-03-28 20:42:18 +0200

    Exercise 2.2 done

--- app/src/main/java/pl/pollub/s95408/lab2/MainActivity.kt
+++ app/src/main/java/pl/pollub/s95408/lab2/MainActivity.kt
@@ -1,19 +1,23 @@
 package pl.pollub.s95408.lab2
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.text.Editable
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,53 +25,45 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val sredniaTextView = findViewById<TextView>(R.id.srednia)
-        sredniaTextView.text = intent.extras?.getString("average")
-        sredniaTextView.visibility = View.VISIBLE
+        val nameTextField = findViewById<EditText>(R.id.nameTextField)
+        val surnameTextField = findViewById<EditText>(R.id.surnameTextField)
+        val marksTextField = findViewById<EditText>(R.id.marksTextField)
+        val resultButton = findViewById<Button>(R.id.resultButton)
+        val average = intent.extras?.getString("average")
+        sredniaTextView.text = getString(R.string.srednia, average)
+        if(intent.extras?.getString("name") != null) {
+            nameTextField.text =
+                Editable.Factory.getInstance().newEditable(intent.extras?.getString("name"))
+            surnameTextField.text =
+                Editable.Factory.getInstance().newEditable(intent.extras?.getString("surname"))
+            marksTextField.text =
+                Editable.Factory.getInstance().newEditable(intent.extras?.getString("marksAmount"))
+        }
+        if(average != null)
+        {
+            sredniaTextView.visibility = View.VISIBLE
+            findViewById<Button>(R.id.marksButton).visibility = View.VISIBLE
+            resultButton.visibility = View.VISIBLE
+            if(BigDecimal(average) < BigDecimal(3.0))
+            {
+                resultButton.text = getString(R.string.buttonNiePoszlo)
+            }
+            else
+            {
+                resultButton.text = getString(R.string.buttonSuper)
+            }
+        }
 
         addListeners()
     }
-/*
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        val nameTextField = findViewById<EditText>(R.id.nameTextField)
-        val surnameTextField = findViewById<EditText>(R.id.surnameTextField)
-        val marksTextField = findViewById<EditText>(R.id.marksTextField)
-        val marksButton = findViewById<Button>(R.id.marksButton)
-        outState.putString("name", nameTextField.text.toString())
-        outState.putString("surname", surnameTextField.text.toString())
-        outState.putString("marks", marksTextField.text.toString())
-        outState.putInt("marksButtonVisibility", marksButton.visibility)
-        outState.putString("marksButtonError", marksButton.error?.toString())
-        outState.putString("nameTextFieldError", nameTextField.error?.toString())
-        outState.putString("surnameTextFieldError", surnameTextField.error?.toString())
-        outState.putString("marksTextFieldError", marksTextField.error?.toString())
-        super.onSaveInstanceState(outState, outPersistentState)
-    }
 
-    override fun onRestoreInstanceState(
-        savedInstanceState: Bundle?,
-        persistentState: PersistableBundle?
-    ) {
-        val nameTextField = findViewById<EditText>(R.id.nameTextField)
-        val surnameTextField = findViewById<EditText>(R.id.surnameTextField)
-        val marksTextField = findViewById<EditText>(R.id.marksTextField)
-        val marksButton = findViewById<Button>(R.id.marksButton)
-        nameTextField.setText(savedInstanceState?.getString("name"))
-        surnameTextField.setText(savedInstanceState?.getString("surname"))
-        marksTextField.setText(savedInstanceState?.getString("marks"))
-        marksButton.visibility = savedInstanceState?.getInt("marksButtonVisibility") ?: View.VISIBLE
-        marksButton.error = savedInstanceState?.getString("marksButtonError")
-        nameTextField.error = savedInstanceState?.getString("nameTextFieldError")
-        surnameTextField.error = savedInstanceState?.getString("surnameTextFieldError")
-        marksTextField.error = savedInstanceState?.getString("marksTextFieldError")
-        super.onRestoreInstanceState(savedInstanceState, persistentState)
-    }
-*/
     private fun addListeners()
     {
         val nameTextField = findViewById<EditText>(R.id.nameTextField)
         val surnameTextField = findViewById<EditText>(R.id.surnameTextField)
         val marksTextField = findViewById<EditText>(R.id.marksTextField)
         val marksButton = findViewById<Button>(R.id.marksButton)
+        val resultButton = findViewById<Button>(R.id.resultButton)
         val fields = listOf(nameTextField, surnameTextField, marksTextField)
         marksButton.setOnClickListener {
             val intent = android.content.Intent(this, MarksActivity::class.java)
@@ -76,6 +72,18 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("surname", surnameTextField.text.toString())
             startActivity(intent)
         }
+        resultButton.setOnClickListener {
+            if(resultButton.text == getString(R.string.buttonNiePoszlo))
+            {
+                Toast.makeText(applicationContext, "Wysyłam podanie o zaliczenie warunkowe", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(applicationContext, "Gratulacje! Otrzymujesz zaliczenie!", Toast.LENGTH_SHORT).show()
+            }
+            this.finishAffinity()
+        }
+
         for(field in fields)
         {
             field.addTextChangedListener { text ->
--- app/src/main/java/pl/pollub/s95408/lab2/MarksActivity.kt
+++ app/src/main/java/pl/pollub/s95408/lab2/MarksActivity.kt
@@ -1,22 +1,26 @@
 package pl.pollub.s95408.lab2
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import java.math.BigDecimal
 import java.math.RoundingMode
-import kotlin.math.roundToInt
 
 class MarksActivity : AppCompatActivity() {
     private lateinit var linearLayoutManager : LinearLayoutManager
     private lateinit var marksAdapter: MarksAdapter
+    private var state: Parcelable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marks2)
-        linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
         val marksRecyclerView = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.marksRecyclerView)
         marksRecyclerView.layoutManager = linearLayoutManager
 
@@ -42,14 +46,23 @@ class MarksActivity : AppCompatActivity() {
             }
             val average = BigDecimal(sum / count).setScale(2, RoundingMode.HALF_EVEN)
             Toast.makeText(this, "Średnia ocen: $average", Toast.LENGTH_SHORT).show()
-            val intent = android.content.Intent(this, MainActivity::class.java)
-            intent.putExtra("average", average.toString())
-            intent.putExtra("marksAmount", marksAdapter.marks.size.toString())
-            intent.putExtra("name", intent.extras?.getString("name"))
-            intent.putExtra("surname", intent.extras?.getString("surname"))
-            setResult(RESULT_OK, intent)
-            startActivity(intent)
+            val backIntent = android.content.Intent(this, MainActivity::class.java)
+            backIntent.putExtra("average", average.toString())
+            backIntent.putExtra("marksAmount", rowCount.toString())
+            backIntent.putExtra("name", intent.extras?.getString("name"))
+            backIntent.putExtra("surname", intent.extras?.getString("surname"))
+            setResult(RESULT_OK, backIntent)
+            startActivity(backIntent)
         }
     }
-
+    override fun onPause()
+    {
+        super.onPause()
+        state = linearLayoutManager.onSaveInstanceState()!!
+    }
+    override fun onResume()
+    {
+        super.onResume()
+        linearLayoutManager.onRestoreInstanceState(state)
+    }
 }
